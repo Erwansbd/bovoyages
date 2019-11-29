394721f57585effd0b64aead258a2173c2dd86e1
jd213057 <jonathan.dimur@gmail.com>
2019-11-29 14:50:04 +0100

    Rest: post de destinations get de destinations valides get de dates de voyages valides post voyage (en cours) get voyage by id
    A faire :
    panier
    creer client
    authentification
    validation du panier

--- src/main/java/fr/gtm/bovoyages/repositories/DatesVoyageRepository.java
+++ src/main/java/fr/gtm/bovoyages/repositories/DatesVoyageRepository.java
@@ -1,11 +1,17 @@
 package fr.gtm.bovoyages.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import fr.gtm.bovoyages.entities.DatesVoyage;
 
 @Repository
+=======
+
+import fr.gtm.bovoyages.entities.DatesVoyage;
+
+>>>>>>> 6acb8a0... Rest:
 public interface DatesVoyageRepository  extends JpaRepository <DatesVoyage, Long> {
 
 }
